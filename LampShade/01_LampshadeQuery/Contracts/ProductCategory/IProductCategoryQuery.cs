f6af3dd1219a1cd98bedafb752c90a3a34fefeac
hamid haghverdi <126750804+hamidhagh@users.noreply.github.com>
2023-12-25 23:07:24 +0330

    Home, footer, header, product category page and search UI implemented

--- LampShade/01_LampshadeQuery/Contracts/ProductCategory/IProductCategoryQuery.cs
+++ LampShade/01_LampshadeQuery/Contracts/ProductCategory/IProductCategoryQuery.cs
@@ -8,6 +8,7 @@ namespace _01_LampshadeQuery.Contracts.ProductCategory
 {
     public interface IProductCategoryQuery
     {
+        ProductCategoryQueryModel GetProductCategoryWithProductsBy(string slug);
         List<ProductCategoryQueryModel> GetProductCategories();
         List<ProductCategoryQueryModel> GetProductCategoriesWithProducts();
     }
---
+++ LampShade/ServiceHost/Pages/Search.cshtml.cs
@@ -0,0 +1,24 @@
+using _01_LampshadeQuery.Contracts.Product;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServiceHost.Pages
+{
+    public class SearchModel : PageModel
+    {
+        private readonly IProductQuery _productQuery;
+        public List<ProductQueryModel> Products;
+        public string Value;
+
+        public SearchModel(IProductQuery productQuery)
+        {
+            _productQuery = productQuery;
+        }
+
+        public void OnGet(string value)
+        {
+            Value = value;
+            Products = _productQuery.Search(value);
+        }
+    }
+}
